fbfeaae4c3afd10dbde03e5a660941a9ed3913f5
Darshika <darshikajha05@gmail.com>
2022-07-19 20:28:07 +0530

    added strategy design pattern

--- src/strategy/StrategyDesignPatternDemo.java
+++ src/strategy/StrategyDesignPatternDemo.java
@@ -5,11 +5,11 @@ public class StrategyDesignPatternDemo {
     public static void main(String[] args) {
         IQuackStrategy simpleQuack = new SimpleQuack();
         IQuackStrategy noQuack = new NoQuack();
-        IFlyStrategy simplefly = new SimpleFly();
+        IFlyStrategy simpleFly = new SimpleFly();
         IFlyStrategy jetFly = new JetFly();
         IDisplayStrategy graphicDisplay = new GraphicDisplay();
         IDisplayStrategy textDisplay = new CommandLineDisplay();
-        Duck simpleDuck = new Duck(simplefly, simpleQuack, textDisplay, "simple Duck");
+        Duck simpleDuck = new Duck(simpleFly, simpleQuack, textDisplay, "simple Duck");
         Duck enhancedDuck = new Duck(jetFly, noQuack, graphicDisplay, "advanced Duck");
         simpleDuck.display();
         enhancedDuck.display();
